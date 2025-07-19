b1cbb114917748debc9f2598f1720f8db1bc0db2
alperenyurtseven <alperenyurtseven@papilon.com.tr>
2025-07-19 23:17:59 +0300

    chore: wipe repo – case-reset

--- v1/src/loaders/index.js
+++
@@ -1,7 +0,0 @@
-const { connectDB } = require('./db');
-const { initializeServiceBus } = require('../services/serviceBus');
-
-module.exports = async () => {
-    await connectDB();
-    await initializeServiceBus();
-}
--- v1/src/validations/putBack.js
+++
@@ -1,9 +0,0 @@
-const Joi = require("joi");
-
-const putBackValidation = Joi.object({
-  deletionId: Joi.string().required().min(36).max(36),
-});
-
-module.exports = {
-  putBackValidation,
-};
